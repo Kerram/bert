44e8275184a40e9950b714d9adf2484a5d2fec66
fiffeek <fm394471@students.mimuw.edu.pl>
2020-05-14 05:52:04 +0200

    a

--- run_classifier.py
+++ run_classifier.py
@@ -953,7 +953,7 @@ def main(_):
 
     result = estimator.predict(input_fn=predict_input_fn)
 
-    output_predict_file = os.path.join(FLAGS.output_dir, "test_results.tsv")
+    output_predict_file = os.path.join(FLAGS.output_dir, "test_results.csv")
     with tf.gfile.GFile(output_predict_file, "w") as writer:
       num_written_lines = 0
       tf.logging.info("***** Predict results *****")
