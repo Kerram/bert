32c8d3ae64585533d58b1a93522e31eca9851a77
Marek Żochowski <marek@zochowski.net>
2020-06-02 20:01:46 +0200

    Add brackets best experiment

--- deephol-tune.sh
+++ deephol-tune.sh
@@ -1,7 +1,7 @@
 sudo python3 run_deephol.py --data_dir=bert/data --bert_config_file=bert/model/bert_config.json \
---vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/models/brackets \
+--vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/models/brackets_best \
 --init_checkpoint=gs://zpp-bucket-1920/bert-bucket-golkarolka/bert_model11/model.ckpt-500000 --num_train_epochs=3.0 \
---max_seq_length=512 --do_train=True --do_eval=True --do_predict=False --do_export=False --use_tpu=True --tpu_name=moje-nie-ruszaj \
+--max_seq_length=512 --do_train=True --do_eval=True --do_predict=False --do_export=False --use_tpu=True --tpu_name=holist-evaluation \
 --gcp_project=zpp-mim-1920 --tpu_zone=us-central1-f --num_tpu_cores 8 --save_checkpoints_steps 10000 \
 --train_file=gs://zpp-bucket-1920/tpu-fine-tune/data/full_train_without_brackets.tf_record \
 --eval_file=gs://zpp-bucket-1920/tpu-fine-tune/data/full_eval_without_brackets.tf_record
