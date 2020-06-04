3ae6c638a8b0083440490e2c57a2164e5344edeb
Marek Żochowski <marek@zochowski.net>
2020-06-04 09:44:03 +0200

    Back to original

--- export_deephol.sh
+++ export_deephol.sh
@@ -1,5 +1,5 @@
 sudo python3 run_deephol.py --data_dir=bert/data --bert_config_file=bert/model/bert_config.json \
---vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/exported/masking_best_real \
---init_checkpoint=gs://zpp-bucket-1920/tpu-fine-tune/models/masking_best_real/model.ckpt-360000 --max_seq_length=512 \
+--vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/exported/masking_best \
+--init_checkpoint=gs://zpp-bucket-1920/tpu-fine-tune/models/masking_best/model.ckpt-360000 --max_seq_length=512 \
 --do_train=False --do_eval=False --do_export=True --use_tpu=False \
 --test_file=gs://zpp-bucket-1920/tpu-fine-tune/data/test.tf_record
