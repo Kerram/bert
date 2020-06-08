3ccc39c36b1900391bc82e35f90ae2955cab4c10
Marek Żochowski <marek@zochowski.net>
2020-06-08 12:06:10 +0200

    Add new converting scripts

---
+++ bert/data/new_test.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+
+train_df = pd.read_csv('test.csv', sep=',').head(1)
+train_df = train_df.replace(['\(', '\)'], ['', ''], regex=True)
+
+counter = {}
+
+for index, row in train_df.iterrows():
+    pair = (row.goal, row.tactic_id)
+    if pair not in counter:
+        counter[pair] = 1
+    else:
+        counter[pair] += 1
+
+    if index % 10000 == 0:
+        print("DONE %d" % (index,))
+
+weights = []
+for index, row in train_df.iterrows():
+    pair = (row.goal, row.tactic_id)
+    weights.append(1/(counter[pair] // 8))
+
+    if index % 10000 == 0:
+        print("DONE %d" % (index,))
+
+train_df = train_df.assign(weight=pd.Series(weights).values)
+train_df = train_df.sample(frac=1).reset_index(drop=True)
+train_df.to_csv('new_test.tsv', index=False, sep='\t')
