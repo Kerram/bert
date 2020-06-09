eb85ba64f3fc29721ce01855aeaaec418f1c65fb
Marek Żochowski <marek@zochowski.net>
2020-06-09 10:31:05 +0200

    Bigger lr

--- debug_finetune.sh
+++ debug_finetune.sh
@@ -1,7 +1,7 @@
 sudo python3 run_deephol.py --data_dir=bert/data --bert_config_file=bert/model/bert_config.json \
---vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/models/new_6_lr \
+--vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/models/new_5_lr \
 --init_checkpoint=gs://zpp-bucket-1920/bert-bucket-golkarolka/bert_model11/model.ckpt-500000 --num_train_epochs=100.0 \
---max_seq_length=512 --do_train=False --do_eval=True --do_export=False --use_tpu=True --tpu_name=bert8 \
+--max_seq_length=512 --do_train=False --do_eval=True --do_export=False --use_tpu=True --tpu_name=bert10 \
 --gcp_project=zpp-mim-1920 --tpu_zone=us-central1-f --num_tpu_cores 8 \
 --eval_file=gs://zpp-bucket-1920/tpu-fine-tune/data/new_mini_valid.tf_record \
 --infinite_eval=True
