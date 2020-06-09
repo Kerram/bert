53aaab6a15bd81e32b4e17d0e59d8cf089166921
Marek Żochowski <marek@zochowski.net>
2020-06-09 13:12:19 +0200

    Bigger lr

--- run_deephol.py
+++ run_deephol.py
@@ -74,7 +74,7 @@ flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
 
 flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
 
-flags.DEFINE_float("learning_rate", 1e-5, "The initial learning rate for Adam.")
+flags.DEFINE_float("learning_rate", 1e-4, "The initial learning rate for Adam.")
 
 flags.DEFINE_float(
     "num_train_epochs", 3.0, "Total number of training epochs to perform."
