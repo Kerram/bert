f96637257208e07c3ccbc175f148ea39c9c10fa5
Marek Żochowski <marek@zochowski.net>
2020-06-10 15:36:37 +0200

    Update GCP path

--- deephol-tune.sh
+++ deephol-tune.sh
@@ -1,7 +1,7 @@
 sudo python3 run_deephol.py --data_dir=bert/data \
---vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/models/wavenet_full_brackets_correct \
+--vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/models/wavenet_losses \
 --num_train_epochs=100.0 \
---max_seq_length=512 --do_train=True --do_eval=True --do_predict=False --do_export=False --use_tpu=True --tpu_name=bert3 \
+--max_seq_length=512 --do_train=True --do_eval=True --do_predict=False --do_export=False --use_tpu=True --tpu_name=bert13 \
 --gcp_project=zpp-mim-1920 --tpu_zone=us-central1-f --num_tpu_cores 8 --save_checkpoints_steps 10000 \
 --train_file=gs://zpp-bucket-1920/tpu-fine-tune/data/train_without_brackets_wavenet.tf_record \
 --eval_file=gs://zpp-bucket-1920/tpu-fine-tune/data/valid_without_brackets_wavenet.tf_record
--- export_deephol.sh
+++ export_deephol.sh
@@ -1,5 +1,5 @@
 sudo python3 run_deephol.py --data_dir=bert/data --bert_config_file=bert/model/bert_config.json \
---vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/exported/wavenet_full_brackets_correct \
---init_checkpoint=gs://zpp-bucket-1920/tpu-fine-tune/models/wavenet_full_brackets_correct/model.ckpt-3330000 --max_seq_length=512 \
+--vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/exported/wavenet_losses \
+--init_checkpoint=gs://zpp-bucket-1920/tpu-fine-tune/models/wavenet_losses/model.ckpt-3330000 --max_seq_length=512 \
 --do_train=False --do_eval=False --do_export=True --use_tpu=False \
 --test_file=gs://zpp-bucket-1920/tpu-fine-tune/data/test_without_brackets_wavenet.tf_record
