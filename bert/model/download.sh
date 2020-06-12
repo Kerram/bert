c99c586a9968b41fd408c5b07ea6bc9e401f16c8
Marek Żochowski <marek@zochowski.net>
2020-06-12 22:02:07 +0200

    Update download of model

--- bert/model/download.sh
+++ bert/model/download.sh
@@ -1,4 +1,2 @@
-BERT_MODEL=bert_model
-
-gsutil cp gs://zpp-bucket-1920/bert-bucket-golkarolka/${BERT_MODEL}/vocab.txt .
-gsutil cp gs://zpp-bucket-1920/bert-bucket-golkarolka/${BERT_MODEL}/bert_config.json .
+gsutil cp gs://zpp-bucket-1920/vocab_ls.txt vocab.txt
+gsutil cp gs://zpp-bucket-1920/bert-bucket-golkarolka/${1}/bert_config.json .
