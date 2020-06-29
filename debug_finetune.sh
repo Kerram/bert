d7fe1feb3cba26cc408360822088e49b7182db5c
Marek Żochowski <marek@zochowski.net>
2020-06-29 14:33:33 +0200

    Add params weight exp

--- debug_finetune.sh
+++ debug_finetune.sh
@@ -1,5 +1,5 @@
 sudo python3 run_deephol.py --data_dir=bert/data \
---vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/models/weighted_beta_wavenet \
+--vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/models/weighted_params_wavenet \
 --num_train_epochs=100.0 \
 --max_seq_length=512 --do_train=False --do_eval=True --do_export=False --use_tpu=True --tpu_name=bert11 \
 --gcp_project=zpp-mim-1920 --tpu_zone=us-central1-f --num_tpu_cores 8 \
