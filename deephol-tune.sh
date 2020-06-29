04d22640f55992c552a9440db847e4879e818614
Marek Żochowski <marek@zochowski.net>
2020-06-29 23:22:38 +0200

    Fix conf

--- deephol-tune.sh
+++ deephol-tune.sh
@@ -1,5 +1,5 @@
 sudo python3 run_deephol.py --data_dir=bert/data \
---vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/models/params_cond_on_tac_bert \
+--vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/models/beta_bert \
 --num_train_epochs=3.0 --bert_config_file=bert/model/bert_config.json \
 --init_checkpoint=gs://zpp-bucket-1920/bert-bucket-golkarolka/bert_model11/model.ckpt-500000 \
 --max_seq_length=512 --do_train=True --do_eval=True --do_predict=False --do_export=False --use_tpu=True --tpu_name=bert10 \
