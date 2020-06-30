4a3b48a813b4ffe0ede635120c970edb557d499b
Marek Żochowski <marek@zochowski.net>
2020-06-30 18:48:55 +0200

    Add conf

--- deephol-tune.sh
+++ deephol-tune.sh
@@ -1,7 +1,7 @@
 sudo python3 run_deephol.py --data_dir=bert/data \
 --vocab_file=bert/model/vocab.txt --output_dir=gs://zpp-bucket-1920/tpu-fine-tune/models/beta_wavenet \
 --num_train_epochs=5.0 \
---max_seq_length=512 --do_train=True --do_eval=True --do_predict=False --do_export=False --use_tpu=True --tpu_name=bert11 \
+--max_seq_length=512 --do_train=True --do_eval=True --do_predict=False --do_export=False --use_tpu=True --tpu_name=bert10 \
 --gcp_project=zpp-mim-1920 --tpu_zone=us-central1-f --num_tpu_cores 8 --save_checkpoints_steps 10000 \
 --train_file=gs://zpp-bucket-1920/tpu-fine-tune/data/preprocessed/train.tf_record \
 --eval_file=gs://zpp-bucket-1920/tpu-fine-tune/data/preprocessed/valid.tf_record
--- run_deephol.py
+++ run_deephol.py
@@ -61,11 +61,11 @@ flags.DEFINE_bool(
 
 flags.DEFINE_string("test_file", None, "Path to test tf_record file. It is used to export model.")
 
-flags.DEFINE_integer("train_batch_size", 32, "Total batch size for training.")
+flags.DEFINE_integer("train_batch_size", 64, "Total batch size for training.")
 
 flags.DEFINE_integer("eval_batch_size", 8, "Total batch size for eval.")
 
-flags.DEFINE_float("learning_rate", 1e-4, "The initial learning rate for Adam.")
+flags.DEFINE_float("learning_rate", 5e-5, "The initial learning rate for Adam.")
 
 flags.DEFINE_float(
     "num_train_epochs", 3.0, "Total number of training epochs to perform."
